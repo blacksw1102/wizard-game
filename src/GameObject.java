a023c273d222ddc99e2b4a02ceb1d94c1955c82f
blacksw1102 <blacksw1102@gmail.com>
2020-11-13 12:45:26 +0900

    업데이트:part 5

---
+++ src/GameObject.java
@@ -0,0 +1,62 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/* 오브젝트의 상태값을 관리하는 클래스 */
+public abstract class GameObject {
+	
+	protected int x, y;
+	protected float velX = 0, velY = 0;
+	protected ID id;
+	
+	public GameObject(int x, int y, ID id) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+	
+	// object update
+	public abstract void tick();
+	public abstract void render(Graphics g);
+	public abstract Rectangle getBounds();
+	
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public float getVelX() {
+		return velX;
+	}
+
+	public void setVelX(float velX) {
+		this.velX = velX;
+	}
+
+	public float getVelY() {
+		return velY;
+	}
+
+	public void setVelY(float velY) {
+		this.velY = velY;
+	}
+	
+}
---
+++ src/ID.java
@@ -0,0 +1,10 @@
+
+
+/* 게임 오브젝트 타입 클래스 */
+public enum ID {
+	Player(),
+	Block(),
+	Crate(),
+	Bullet(),
+	Enemy();
+}
